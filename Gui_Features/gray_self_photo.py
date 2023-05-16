62d65c0d1ba11fac9c53c14bcadd02c75a018d5b
[chminsta] <[lcentimeter@hanyang.ac.kr]>
2023-05-16 21:29:55 +0900

    gray self photo

--- Gui_Features/gray_self_photo.py
+++ Gui_Features/gray_self_photo.py
@@ -1,10 +1,17 @@
 import numpy as np
 import cv2 as cv
 import datetime
+import os
 
 cap = cv.VideoCapture(0)
 i = 1
 selected_images = []
+try:
+    os.mkdir('gray')
+finally:
+    print("gray directory founded")
+    
+
 
 if not cap.isOpened():
     print("Cannot open camera")
@@ -30,7 +37,7 @@ while True:
             selected_images = [cv.resize(img, (0, 0), fx=0.5, fy=0.5) for img in selected_images]
             montage = np.hstack(selected_images)
             timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-            result_filename = 'opencv_python_practice/Gui_Features/gray/result_' + timestamp + '.jpg'
+            result_filename = 'gray/result_' + timestamp + '.jpg'
             cv.imshow('Montage', montage)
             cv.imwrite(result_filename, montage)
             cv.waitKey(0)
@@ -42,14 +49,14 @@ while True:
         else:
             montage = np.vstack(selected_images)
             timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-            result_filename = 'opencv_python_practice/Gui_Features/gray/result_' + timestamp + '.jpg'
+            result_filename = 'gray/result_' + timestamp + '.jpg'
             cv.imshow('Montage', montage)
             cv.imwrite(result_filename, montage)
             cv.waitKey(0)
             break
 
     if key == ord('s'):
-        cv.imwrite('opencv_python_practice/Gui_Features/gray/' + str(i) + '.jpg', gray)
+        cv.imwrite('gray/' + str(i) + '.jpg', gray)
         cv.imshow('gray' + str(i), gray)
         i += 1
         selected_images.append(gray)
