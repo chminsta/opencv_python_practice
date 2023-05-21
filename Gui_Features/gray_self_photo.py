817d3260a3842bd296d85b7bce235b61ab572f05
[chminsta] <[lcentimeter@hanyang.ac.kr]>
2023-05-21 17:22:07 +0900

    photo update

--- Gui_Features/gray_self_photo.py
+++ Gui_Features/gray_self_photo.py
@@ -6,12 +6,11 @@ import os
 cap = cv.VideoCapture(0)
 i = 1
 selected_images = []
-#try:
-#    os.mkdir('gray')
-#finally:
-#    print("gray directory founded")
-    
 
+if not os.path.exists('photo'):
+    os.makedirs('photo')
+    
+letter = "Press 's' to capture, 'c' to cancel." 
 
 if not cap.isOpened():
     print("Cannot open camera")
@@ -24,44 +23,73 @@ while True:
     if not ret:
         print("Can't receive frame (stream end?). Exiting...")
         break
+    letter2 = "Captured "+str(i-1)+" images"
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow('frame', gray)
+    show = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.putText(show , letter, (10, 100), cv.FONT_ITALIC, 1, (0, 255, 0))
+    cv.putText(show , letter2, (10, 400), cv.FONT_ITALIC, 1, (0, 255, 0))
+    cv.imshow('camera', show)
 
     key = cv.waitKey(1)
+    
+
+
+
 
     if key == ord('q'):
         if len(selected_images) < 4:
-            print("Select at least four images by pressing 's' before 'q'.")
+            letter = "Select at least four images by pressing 's' before 'q'."
         else:
             selected_images = [cv.resize(img, (0, 0), fx=0.5, fy=0.5) for img in selected_images]
             montage = np.hstack(selected_images)
             timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-            result_filename = 'gray/result_' + timestamp + '.jpg'
-            cv.imshow('Montage', montage)
+            result_filename = 'photo/result_' + timestamp + '.jpg'
+            cv.imshow('result', montage)
             cv.imwrite(result_filename, montage)
             cv.waitKey(0)
             break
 
     if key == ord('w'):
         if len(selected_images) < 4:
-            print("Select at least four images by pressing 's' before 'w'.")
+            letter = "Select at least four images by pressing 's' before 'q'."
         else:
+            
             montage = np.vstack(selected_images)
             timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-            result_filename = 'gray/result_' + timestamp + '.jpg'
+            result_filename = 'photo/result_' + timestamp + '.jpg'
             cv.imshow('Montage', montage)
             cv.imwrite(result_filename, montage)
             cv.waitKey(0)
             break
 
     if key == ord('s'):
-        cv.imwrite('gray/' + str(i) + '.jpg', gray)
+        letter = "Press 's' to capture, 'c' to cancel." 
+        #cv.imwrite('gray/' + str(i) + '.jpg', gray)
         cv.imshow('gray' + str(i), gray)
         i += 1
         selected_images.append(gray)
+        cv.imshow('selected',np.hstack([cv.resize(img, (0, 0), fx=0.5, fy=0.5) for img in selected_images]))
+        
         if len(selected_images) >= 4:
-            print("Press 'q' to display the selected images horizontally or 'w' to display them vertically.")
+            letter = "Press 'q' : horizontally or 'w' : vertically"
+    
+    if key == ord('c'):
+        if i==1:
+            letter = "no photo to cancel"
+            
+        else:
+            i -= 1
+            selected_images.pop()
+            cv.destroyWindow('gray' + str(i)) 
+            if i != 1:
+                cv.imshow('selected',np.hstack([cv.resize(img, (0, 0), fx=0.5, fy=0.5) for img in selected_images]))
+            else:
+                cv.destroyWindow('selected')
+        if len(selected_images) < 4:
+            letter = "Press 's' to capture, 'c' to cancel."
 
+        
+os.startfile('photo')
 cap.release()
 cv.destroyAllWindows()
